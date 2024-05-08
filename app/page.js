0cda874fae33b42cf28c9377ea0cbf27d3e37192
wpocoyo87 <khadijah.lkc@gmail.com>
2024-05-09 02:44:26 +0800

    I change the content

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Change Your Website Here&nbsp;
+          Can Change My Line Now&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
