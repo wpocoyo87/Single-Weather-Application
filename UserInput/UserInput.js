2bc4256d66e581412471ae360214906915da613a
wpocoyo87 <khadijah.lkc@gmail.com>
2024-05-09 15:12:35 +0800

    Update Page.js & UserInput

---
+++ UserInput/UserInput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Userinput = () => {
+    return (
+        <div>
+            <label>
+                <span>Latitude</span>
+                <input type={"text"} placeholder={"Latitude"}/>
+
+            </label>
+            
+        </div>
+    );
+};
+
+export default Userinput;
